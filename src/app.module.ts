b3813e32ba04bde5977739acba12b3e41e7102eb
Prashant Pandey <prashant.pandey.dev@gmail.com>
2020-03-06 19:24:22 -0500

    auth api implemented

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { SharedModule } from './shared/shared.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    SharedModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
